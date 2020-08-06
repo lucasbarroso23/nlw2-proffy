704d95620fe763f515c4648f5d6d6ea9340d169b
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-06 11:06:54 -0300

    finalizando o terceiro dia da nlw, integração entre front e back concluída, partiu mobileeee maluco

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,32 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/55262061?s=460&u=afd5e36a1928c45b184a72081834fe19a88b7046&v=4" alt="Lucas Barroso" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Lucas Barroso</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiata das melhores tecnologias de química avançada.
-            <br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vidas das pessoas através de experiências.Mais de 200.00 pessoas já passaram por uma das minhas explosões
-        </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                <strong>R$ 50,00</strong>
+    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank"
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Wahtsapp" />
                 Entrar em contato
-            </button>
+            </a>
             </footer>
         </article>
     )
